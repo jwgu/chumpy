b25cfc2136116247edd0ad69b7ffe47e0a59879c
Jinwei Gu <gujinwei@gmail.com>
2018-09-18 16:01:04 -0700

    python3 support

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ See LICENCE.txt for licensing and contact information.
 """
 
 from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip.req import parse_requirements
 from runpy import run_path
 
 install_reqs = parse_requirements('requirements.txt', session=False)
